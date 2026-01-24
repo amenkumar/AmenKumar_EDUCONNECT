3ec11e7eff6d56a8b98a74e9cb41f03b29245485
amenkumar <dummy@gmail.com>
2026-01-24 11:50:09 +0000

    day 8 updated

--- server/src/main/java/com/edutech/progressive/repository/CourseRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/CourseRepository.java
@@ -1,10 +1,29 @@
 package com.edutech.progressive.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edutech.progressive.entity.Course;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course,Integer>{
+
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+
+    @Query("SELECT c FROM Course c WHERE c.courseId = :courseId")
+    public Course findByCourseId(@Param("courseId") int courseId);
+    
+    @Query("SELECT c FROM Course c WHERE c.teacher.teacherId = :teacherId")
+    public List<Course> findAllByTeacherId(@Param("teacherId") int teacherId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Course c WHERE c.teacher.teacherId = :teacherId")
+    public void deleteByTeacherId(@Param("teacherId") int teacherId);
+
 }
